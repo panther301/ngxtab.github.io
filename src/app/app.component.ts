ddc06b52f3dc13d51e761849a5b6c07b65bcc280
Anindya Giri <anindya@simplifyworkforce.com>
2020-09-04 18:17:53 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TabGroupConfig } from 'ngx-tab-group';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ngxtab';
+  title = 'ngx-tab-group';
+  tabIndex = 0
+  // total tab we coded in the html. this one for previous and next functionality.
+  totalTab = 4
+
+  text1 = "import { NgxTabGroupModule } from 'ngx-tab-group'"
+
+  config: TabGroupConfig = {
+    position: 'horizontal',
+    // coClass: 'tab-panel'
+  }
+
+  config1: TabGroupConfig = {
+    position: 'horizontal',
+    coClass: 'tab-panel'
+  }
+
+  config2: TabGroupConfig = {
+    position: 'horizontal',
+    coClass: 'tab-panel'
+  }
+  tIndex = 1
+
+
+  onClick() {
+    if (this.config1.position === 'horizontal') {
+      this.config1.position = 'vertical'
+    } else {
+      this.config1.position = 'horizontal'
+    }
+  }
+
+  onPre() {
+    if (this.tabIndex === 0) {
+      return;
+    }
+    this.tabIndex = this.tabIndex - 1
+  }
+
+  onNext() {
+    if (this.tabIndex === this.totalTab) {
+      return;
+    }
+    this.tabIndex = 1 + this.tabIndex
+  }
+
+  onIndexChange(event) {
+    this.tabIndex = event
+    // console.log(event)
+  }
 }
